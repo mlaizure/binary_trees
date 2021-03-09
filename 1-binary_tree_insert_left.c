f2d8760a394b63ddad9033cef69e7e4a5b5a5472
mlaizure <maddi.laizure@gmail.com>
2021-03-09 18:44:12 +0000

    Fixed typo

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
 	{
