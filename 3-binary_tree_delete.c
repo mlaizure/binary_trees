d7ff91f16d4ec1da61ed7795106af5398c639d2f
mlaizure <maddi.laizure@gmail.com>
2021-03-09 17:32:01 +0000

    Task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes a binary tree
+ * @tree - pointer to root node
+ * Return: none
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
