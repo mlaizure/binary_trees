20131bd26662cd979c8b03fd81beac79e61e4890
mlaizure <maddi.laizure@gmail.com>
2021-03-10 06:12:51 +0000

    Added NULL checks

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,9 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node->left && !node->right && node->parent != NULL)
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
 		return (1);
 	return (0);
 }
